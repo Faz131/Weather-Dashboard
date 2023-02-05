25c6287f54facc6da9931f6a983193f25bb1878f
Faz131 <fazms@icloud.com>
2023-02-05 15:30:17 -0700

    tried to get the future weather information that was pulled in from OpenWeatherMap to display.  Tried to get icons that respresent conditions to display

--- jscopy.js
+++ jscopy.js
@@ -1,11 +1,16 @@
-// var displayLocal = document.querySelector('.rowsearch');
+
 
 function getWeather(cityID) {
+
     var key = '68fb699c2ddc146cb7053a0a93b881bd'
+
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
+
+        // fetch(url)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
-            displayWeather(data);
+            (displayWeather(data));
+
             console.log(data);
         })
         .catch(function () {
@@ -25,13 +30,65 @@ function displayWeather(d) {
     document.getElementById('tempEL').innerHTML = fahrenheit + '&deg;';
     document.getElementById('cityEL').innerHTML = d.name;
 
-    if (description.indexOf('sunny') > 0) {
-        document.body.className = 'sunny';
-    } else if (description.indexOf('cloudy') > 0) {
-        document.body.className = 'cloudy';
-    } else if (description.indexOf('rainy') > 0) {
-        document.body.className = 'rainy;'
-    }
 
-    // console.log(displayWeather);
+
+    var rightNow = dayjs().format(' ' + '(' + 'MMM DD, YYYY' + ')');
+    $('#cityEL').append(rightNow);
+
+
+
+
 }
+
+function weatherForecast(cityID) {
+
+}
+
+var forecastkey = '68fb699c2ddc146cb7053a0a93b881bd'
+var url = 'http://api.openweathermap.org/data/2.5/forecast?id=5318313&appid=' + forecastkey;
+
+fetch(url)
+    .then(function (resp) {
+
+        return resp.json();
+    })
+    .then(function (resp) {
+        console.log(resp)
+    })
+
+
+
+    .catch(console.err)
+
+// var forecastkey = '68fb699c2ddc146cb7053a0a93b881bd'
+// var url = 'http://api.openweathermap.org/data/2.5/forecast?id=5318313&appid=' + forecastkey;
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         var iconCode = data.weather[0].icon;
+//         // Use the iconCode to fetch the icon
+//     })
+//     .catch(error => {
+//         // Create a new image element
+//         var iconCode = document.createElement("img");
+
+//         // Set the source URL of the icon
+//         iconCode.src = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+
+//         // Set the alt text for the icon
+//         iconCode.alt = "Weather icon";
+
+//         // Append the icon to the HTML DOM
+//         document.body.appendChild(iconCode);
+//     });
+
+function displayForecast(f) {
+
+
+    var weatherRow = document.getElementById('weatherRow');
+    weatherRow.innerHTML = resp.daily.map(day => {
+        return '<p>Day</p>';
+    })
+        .join('');
+}
+
